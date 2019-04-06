5e2ca1a310b3e5837885c5a7d6981aebdb544031
Akash Jain <10014285@sbstudents.org>
2019-04-06 15:36:29 -0400

    Joystick

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour {
 
     Rigidbody2D rb;
+    protected Joystick joystick;
     public GameObject butL;
     public GameObject butR;
     public GameObject butU;
@@ -13,16 +14,17 @@ public class Movement : MonoBehaviour {
     private float moveV;
     private float factorH;
     private float factorV;
-    public float moveSpeed = 100;
     public bool left = false;
     public bool right = false;
-    public bool up = false;
     public bool down = false;
+    public bool up = false;
+    public float moveSpeed = 100;
 
     // Use this for initialization
     void Start () {
 
         rb = GetComponent<Rigidbody2D>();
+        joystick = FindObjectOfType<Joystick>();
         Input.ResetInputAxes();
 
     }
@@ -30,37 +32,28 @@ public class Movement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if(Input.GetKeyUp(KeyCode.LeftArrow) && Input.GetKeyUp(KeyCode.A))
-        {
-            Input.ResetInputAxes();
-        }
-        if (Input.GetKeyUp(KeyCode.RightArrow) && Input.GetKeyUp(KeyCode.D))
-        {
-            Input.ResetInputAxes();
-        }
-        if (Input.GetKeyUp(KeyCode.UpArrow) && Input.GetKeyUp(KeyCode.W))
-        {
-            Input.ResetInputAxes();
-        }
-        if (Input.GetKeyUp(KeyCode.DownArrow) && Input.GetKeyUp(KeyCode.S))
-        {
-            Input.ResetInputAxes();
-        }
         factorH = 0;
         factorV = 0;
-        if (Input.GetAxis("Horizontal") == -1)
+        if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A)
+            || Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D)
+            || Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W)
+            || Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S))
+        {
+            Input.ResetInputAxes();
+        }
+        if (Input.GetAxis("Horizontal") <= -1)
         {
             factorH = -1;
         }
-        if (Input.GetAxis("Horizontal") == 1)
+        if (Input.GetAxis("Horizontal") >= 1)
         {
             factorH = 1;
         }
-        if (Input.GetAxis("Vertical") == -1)
+        if (Input.GetAxis("Vertical") <= -1)
         {
             factorV = -1;
         }
-        if (Input.GetAxis("Vertical") == 1)
+        if (Input.GetAxis("Vertical") >= 1)
         {
             factorV = 1;
         }
@@ -80,20 +73,18 @@ public class Movement : MonoBehaviour {
         {
             factorV = 1;
         }
+        if (joystick.Horizontal > 0 || joystick.Horizontal < 0)
+        {
+            factorH = joystick.Horizontal;
+        }
+        if (joystick.Vertical > 0 || joystick.Vertical < 0)
+        {
+            factorV = joystick.Vertical;
+        }
         moveH = factorH * moveSpeed;
         moveV = factorV * moveSpeed;
         rb.velocity = new Vector2(moveH, moveV);
 
     }
-
-    public void setLeftTrue() { left = true; }
-    public void setLeftFalse() { left = false; }
-    public void setRightTrue() { right = true; }
-    public void setRightFalse() { right = false; }
-    /*public void setUpTrue() { left = true; }
-    public void setLeftFalse() { left = false; }
-    public void setRightTrue() { right = true; }
-    public void setRightFalse() { right = false; }*/
-    //a
 
 }
