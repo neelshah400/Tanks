4a88dc3cd867a7e29a09510697dedf02e3d46ab7
Akash Jain <10014285@sbstudents.org>
2019-04-06 21:18:20 -0400

    final but not really final

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,86 +5,37 @@ using UnityEngine;
 public class Movement : MonoBehaviour {
 
     Rigidbody2D rb;
-    protected Joystick joystick;
-    public GameObject butL;
-    public GameObject butR;
-    public GameObject butU;
-    public GameObject butD;
-    private float moveH;
-    private float moveV;
-    private float factorH;
-    private float factorV;
-    public bool left = false;
-    public bool right = false;
-    public bool down = false;
-    public bool up = false;
-    public float moveSpeed = 100;
+    public float moveSpeed = 0;
 
     // Use this for initialization
     void Start () {
 
         rb = GetComponent<Rigidbody2D>();
-        joystick = FindObjectOfType<Joystick>();
+        //joystick = FindObjectOfType<Joystick>();
         Input.ResetInputAxes();
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-
-        factorH = 0;
-        factorV = 0;
-        if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A)
-            || Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D)
-            || Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W)
-            || Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S))
-        {
-            Input.ResetInputAxes();
-        }
-        if (Input.GetAxis("Horizontal") <= -1)
-        {
-            factorH = -1;
-        }
-        if (Input.GetAxis("Horizontal") >= 1)
-        {
-            factorH = 1;
-        }
-        if (Input.GetAxis("Vertical") <= -1)
-        {
-            factorV = -1;
-        }
+        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+        Vector2 mouseOnScreen = (Vector2)Camera.main.WorldToViewportPoint(Input.mousePosition);
+        float angle = 90 + Mathf.Atan2(positionOnScreen.y - mouseOnScreen.y, positionOnScreen.x - mouseOnScreen.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
         if (Input.GetAxis("Vertical") >= 1)
         {
-            factorV = 1;
+            moveSpeed = 200;
         }
-        if (left == true)
+        else
         {
-            factorH = -1;
+            moveSpeed = 0;
         }
-        if (right == true)
-        {
-            factorH = 1;
-        }
-        if (down == true)
-        {
-            factorV = -1;
-        }
-        if (up == true)
-        {
-            factorV = 1;
-        }
-        if (joystick.Horizontal > 0 || joystick.Horizontal < 0)
-        {
-            factorH = joystick.Horizontal;
-        }
-        if (joystick.Vertical > 0 || joystick.Vertical < 0)
-        {
-            factorV = joystick.Vertical;
-        }
-        moveH = factorH * moveSpeed;
-        moveV = factorV * moveSpeed;
-        rb.velocity = new Vector2(moveH, moveV);
+        rb.AddRelativeForce(new Vector2(0, moveSpeed));
 
     }
 
+    public Vector2 sendPosition()
+    {
+        return Camera.main.WorldToViewportPoint(transform.position);
+    }
 }
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Movement mv;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        rb = GetComponent<Rigidbody2D>();
+        rb.gameObject.SetActive(false);
+        mv = GameObject.FindObjectOfType(typeof(Movement)) as Movement;
+        Input.ResetInputAxes();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 start = new Vector2();
+        Debug.Log("does this even pop up plz god");
+        start = mv.sendPosition();
+        transform.position = start;
+        Vector2 mouseOnScreen = (Vector2)Camera.main.WorldToViewportPoint(Input.mousePosition);
+        float angle = 90 + Mathf.Atan2(start.y - mouseOnScreen.y, start.x - mouseOnScreen.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        if (Input.GetKeyUp(KeyCode.Mouse1)){
+            rb.gameObject.SetActive(true);
+            GameObject shot = Instantiate(rb.gameObject, transform.position, transform.rotation);
+            shot.GetComponent<Rigidbody>().AddForce(transform.forward * 100);
+        }
+
+    }
+}
